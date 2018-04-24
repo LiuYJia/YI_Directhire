12a455d1e73f6666fc0a6705dc6d7dca30186832
fengluluf <2277196352@qq.com>
2018-04-24 11:11:52 +0800

    创建登录、注册、密码找回页面

---
+++ foreend/jobseeker/src/pages/pwback/pwback.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PwbackPage } from './pwback';
+
+@NgModule({
+  declarations: [
+    PwbackPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PwbackPage),
+  ],
+})
+export class PwbackPageModule {}
