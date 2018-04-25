0b5c528ade11aa0ec32d55895952f8bd84fb87d8
fengluluf <2277196352@qq.com>
2018-04-25 16:58:47 +0800

    完成登录页面并上传部分图片

--- foreend/src/app/app.component.ts
+++ foreend/src/app/app.component.ts
@@ -5,13 +5,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { RectabsPage } from '../pages/rectabs/rectabs';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   //rootPage:any = TabsPage;
-  rootPage:any = RectabsPage;
+  //rootPage:any = RectabsPage;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
