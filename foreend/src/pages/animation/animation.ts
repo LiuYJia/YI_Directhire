27481e8ebc04c8df86fa8495754eb90abf30b09e
xingjialei <2461539948@qq.com>
2018-05-02 16:14:45 +0800

    "2018-5-2 Choose选择用户页面"

---
+++ foreend/src/pages/animation/animation.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ChoosePage } from '../choose/choose';
+
+/**
+ * Generated class for the AnimationPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-animation',
+  templateUrl: 'animation.html',
+})
+export class AnimationPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AnimationPage');
+  }
+
+  goPage(){
+    this.navCtrl.push(ChoosePage);
+  }
+
+}
