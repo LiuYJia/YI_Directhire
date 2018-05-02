297825d41ba109c67a5cdb0d424d3c803a8687eb
xingjialei <2461539948@qq.com>
2018-05-02 16:17:31 +0800

    "2018-5-2 Choose选择用户页面(2)"

--- foreend/src/pages/choose/choose.ts
+++ foreend/src/pages/choose/choose.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from '../../pages/login/login';
 
 /**
  * Generated class for the ChoosePage page.
@@ -23,10 +24,10 @@ export class ChoosePage {
   }
 
   goPage01(){
-    this.navCtrl.push('LoginPage');
+    this.navCtrl.push(LoginPage);
   }
 
   goPage02(){
-    this.navCtrl.push('LoginPage');
+    this.navCtrl.push(LoginPage);
   }
 }
