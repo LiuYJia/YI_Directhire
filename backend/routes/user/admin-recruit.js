610558efbf678626e53970205209993b173ffa47
fengluluf <2277196352@qq.com>
2018-05-11 09:20:00 +0800

    用户信息修改

--- backend/routes/user/admin-recruit.js
+++ backend/routes/user/admin-recruit.js
@@ -17,6 +17,7 @@ router.get('/', function(req, res) {
     console.log('userpassword'+param.userPwd);
     var userName = param.userName;
     var userPwd = param.userPwd;
+    var status = param.status;
 
     //密码加密
     //let md5 = crypto.createHash("md5");
@@ -25,33 +26,54 @@ router.get('/', function(req, res) {
     //创建UserS对象
     var newUser = new AdminR({
       username: userName,
-      password: userPwd
+      password: userPwd,
+      status: status
     });
 
     //检查用户名是否存在
     AdminR.getUserNumByName(newUser.username, function (err, results) { 
       
       console.log(results[0]['num']);
-      console.log('22222222222222222222222222222'+newUser.username);
 
         if(newUser.username != undefined && newUser.username != ''){
-          if(results[0]['num'] == 0){
-            console.log('success!');
-    
-            //向数据库存储数据
-            newUser.save({username:newUser.username,userpass:newUser.password},function(err,result){
-              if(err){
-                res.locals.error = err;
-                return;
-              }
-    
-            })
-            //返回响应数据
-            res.send('0');  
+          //注册用户
+          if(newUser.status === 'register'){
+            if(results[0]['num'] == 0){
+              console.log('success!');
+      
+                //向数据库存储数据
+                newUser.save(this,function(err,result){
+                  if(err){
+                    res.locals.error = err;
+                    return;
+                  }
+               })
+                //返回响应数据
+                res.send('0');
+            }
+            if(results[0]['num'] > 0){
+              res.send('1');
+              console.log('用户名已存在');
+            }
           }
-          if(results[0]['num'] > 0){
-            res.send('1');
-            console.log('用户名已存在');
+          //登录
+          else if(newUser.status === 'login'){
+              if(results[0]['num'] == 0){
+                res.send('该用户不存在')
+              }
+              else if(results[0]['num'] > 0){
+                //获取用户信息
+                AdminR.getUserByUserName(newUser.username,function(err,results){
+                  console.log(results);
+                  console.log(results[0]['password']);
+                  if(newUser.password === results[0]['password']){
+                    res.send('登录成功');
+                  }
+                  else{
+                    res.send('用户名或密码不正确');
+                  }
+                })
+              }
           }
         }
         else{
--- backend/routes/user/admin-seeker.js
+++ backend/routes/user/admin-seeker.js
@@ -17,6 +17,7 @@ router.get('/', function(req, res) {
     console.log('userpassword'+param.userPwd);
     var userName = param.userName;
     var userPwd = param.userPwd;
+    var status = param.status;
 
     //密码加密
     //let md5 = crypto.createHash("md5");
@@ -25,7 +26,8 @@ router.get('/', function(req, res) {
     //创建UserS对象
     var newUser = new AdminS({
       username: userName,
-      password: userPwd
+      password: userPwd,
+      status: status
     });
 
     //检查用户名是否存在
@@ -33,24 +35,45 @@ router.get('/', function(req, res) {
       
       console.log(results[0]['num']);
 
-        if(newUser.username != '' && newUser.username != undefined){
-          if(results[0]['num'] == 0){
-            console.log('success!');
-    
-            //向数据库存储数据
-            newUser.save({username:newUser.username,userpass:newUser.password},function(err,result){
-              if(err){
-                res.locals.error = err;
-                return;
-              }
-    
-            })
-            //返回响应数据
-            res.send('0');  
+        if(newUser.username != undefined && newUser.username != ''){
+          //注册用户
+          if(newUser.status === 'register'){
+            if(results[0]['num'] == 0){
+              console.log('success!');
+      
+                //向数据库存储数据
+                newUser.save(this,function(err,result){
+                  if(err){
+                    res.locals.error = err;
+                    return;
+                  }
+               })
+                //返回响应数据
+                res.send('0');
+            }
+            if(results[0]['num'] > 0){
+              res.send('1');
+              console.log('用户名已存在');
+            }
           }
-          if(results[0]['num'] > 0){
-            res.send('1');
-            console.log('用户名已存在');
+          //登录
+          else if(newUser.status === 'login'){
+              if(results[0]['num'] == 0){
+                res.send('该用户不存在')
+              }
+              else if(results[0]['num'] > 0){
+                //获取用户信息
+                AdminS.getUserByUserName(newUser.username,function(err,results){
+                  console.log(results);
+                  console.log(results[0]['password']);
+                  if(newUser.password === results[0]['password']){
+                    res.send('登录成功');
+                  }
+                  else{
+                    res.send('用户名或密码不正确');
+                  }
+                })
+              }
           }
         }
         else{
