b1528ef7101d91ed4aaf4632d056fbebcd6c92ab
fengluluf <2277196352@qq.com>
2018-05-16 14:38:57 +0800

    我的及设置页面信息获取

--- foreend/src/pages/recmy/recmy.ts
+++ foreend/src/pages/recmy/recmy.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams,App } from 'ionic-angular';
 import { LoginPage } from '../login/login';
 import { RecloginPage } from '../reclogin/reclogin';
+import { Http } from '@angular/http';
 
 /**
  * Generated class for the RecmyPage page.
@@ -17,15 +18,21 @@ import { RecloginPage } from '../reclogin/reclogin';
 })
 export class RecmyPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public app:App) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public app:App,public http:Http) {
   }
 
+  username = '';
   ionViewDidLoad() {
     console.log('ionViewDidLoad RecmyPage');
+    console.log(localStorage.getItem("login"));
+    this.username = localStorage.getItem("login");
+    this.http.get('http://127.0.0.1:3000/user/getMessage_recruit?userName='+localStorage.getItem("login")).subscribe(data=>{
+      console.log(data);
+    });
   }
 
   items = ["发布职位信息","我发布的","清除缓存"];
-  user = {img:"assets/imgs/class3.png",name:"Luna",num:"13145878888",password:"123456"};
+  user = {img:"assets/imgs/class3.png",username:"a",name:"Luan",num:"13145878888",job:"人事经理",age:"25岁",email:"123@163.com",password:"123456"};
   
   set(){
     this.navCtrl.push('RecsetPage',{user:this.user});
@@ -41,5 +48,6 @@ export class RecmyPage {
   }
   leave(){
     this.app.getRootNavs()[0].setRoot(RecloginPage);
+    localStorage.removeItem('login');
   }
 }
--- foreend/src/pages/recset/recset.ts
+++ foreend/src/pages/recset/recset.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams,Platform, ToastController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { AlertController } from 'ionic-angular';
 
 
 /**
@@ -15,20 +17,52 @@ import { IonicPage, NavController, NavParams,Platform, ToastController } from 'i
   templateUrl: 'recset.html',
 })
 export class RecsetPage {
-  name;img;num;password;
+  username='';name;img;phone;job;age;email;password;
 
   constructor(public platform: Platform,
     public toastCtrl: ToastController,
     public navCtrl: NavController, 
-    public navParams: NavParams) {
-      this.name = navParams.get('user').name;
-      this.img = navParams.get('user').img;
-      this.num = navParams.get('user').num;
-      this.password = navParams.get('user').password;
+    public navParams: NavParams,
+    public http: Http,
+    public alertCtrl: AlertController) {
+      //this.username = navParams.get('user').username;
+      // this.name = navParams.get('user').name;
+      // this.img = navParams.get('user').img;
+      // this.phone = navParams.get('user').num;
+      // this.job = navParams.get('user').job;
+      // this.age = navParams.get('user').age;
+      // this.email = navParams.get('user').email;
+      // this.password = navParams.get('user').password;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RecsetPage');
+    console.log(localStorage.getItem('login'));
+    this.username = localStorage.getItem('login');
+    this.http.get('http://127.0.0.1:3000/user/getMessage_recruit?userName='+localStorage.getItem("login")).subscribe(data=>{
+      var message = JSON.parse(data['_body'])[0];
+      console.log(message);
+      this.name = message.name;
+      this.phone = message.phone;
+      this.job = message.job;
+      this.age = message.age;
+      this.email = message.email;
+      this.password = message.password;
+    });
+  }
+  showAlert1() {
+    let alert = this.alertCtrl.create({
+      subTitle: '修改成功！',
+      buttons: ['确定']
+    });
+   alert.present();
+  } 
+  showAlert2() {
+    let alert = this.alertCtrl.create({
+      subTitle: '修改失败！',
+      buttons: ['确定']
+    });
+   alert.present();
   }
   back(){
     this.navCtrl.pop();
@@ -37,5 +71,11 @@ export class RecsetPage {
   cgpwd(){
     this.navCtrl.push('ReccgpwdPage');
   }
-
+  submit(){
+    this.http.get('http://127.0.0.1:3000/user/updateRecruit?username='+localStorage.getItem("login")+'&name='+this.name+'&job='+this.job+'&age='+this.age+'&password='+this.password+'&phone='+this.phone+'&email='+this.email).subscribe(data=>{
+      console.log(data);
+      if(data['_body']==1) this.showAlert1();
+      if(data['_body']==2) this.showAlert2();
+    });
+  }
 }
